39355f2f6c424761b3dc9f8e1c4d6689e310f180
blfrie <blfrie@ed05e26c-5bc3-11de-8832-3f4c773c73ae>
2009-06-20 03:50:27 +0000

    Removed CLS Compliance flag.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,6 +29,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
-
-// This assembly exposes external types
-[assembly: CLSCompliantAttribute(true)]
