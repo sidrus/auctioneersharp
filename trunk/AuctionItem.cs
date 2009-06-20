7e4df38c737e6556abf8794eec5c643a60ab2546
blfrie <blfrie@ed05e26c-5bc3-11de-8832-3f4c773c73ae>
2009-06-20 03:50:56 +0000

    Fixed StyleCop and FxCop warnings.

--- trunk/AuctionItem.cs
+++ trunk/AuctionItem.cs
@@ -1,56 +1,128 @@
-/*
- * Created by SharpDevelop.
- * User: Brandon
- * Date: 6/17/2009
- * Time: 11:14 PM
- */
-using System;
-using LuaInterface;
-
+//-----------------------------------------------------------------------
+// <copyright file="AuctionItem.cs" company="Ejafi Software">
+//      Copyright (c) 2009 All Right Reserved
+// </copyright>
+// <author>Brandon Frie</author>
+// <date>6/19/2009</date>
+// <summary>
+//      Defines an individual item in an Auctioneer scan image.
+// </summary>
+//-----------------------------------------------------------------------
 namespace AuctioneerSharp
 {
+    using System;
+    using LuaInterface;
+
+    /// <summary>
+    /// Defines an individual item in an Auctioneer scan image.
+    /// </summary>
     public class AuctionItem
     {
-        private LuaTable itemData;
-
+        /// <summary>
+        /// A WoW-formatted string that defines a link to the item in-game.
+        /// </summary>
         private string itemWowLink;
-        public string ItemWowLink {
-            get {
-                return itemWowLink.Replace("|", "\\124");;
-            }
-        }
+        
+        /// <summary>
+        /// The item's level.
+        /// </summary>
         private int itemLevel;
-        public int ItemLevel {
-            get { return itemLevel; }
-        }
-        private string type;
-        public string Type {
-            get { return type; }
-        }
+        
+        /// <summary>
+        /// The main item type.
+        /// </summary>
+        private string itemType;
+        
+        /// <summary>
+        /// The item subtype.
+        /// </summary>
         private string subType;
-        public string SubType {
-            get { return subType; }
-        }
+        
+        /// <summary>
+        /// The slot the item can occupy.
+        /// </summary>
         private int equipmentSlot;
-        public int EquipmentSlot {
-            get { return equipmentSlot; }
-        }
+        
+        /// <summary>
+        /// The item's name.
+        /// </summary>
         private string name;
-        public string Name {
-            get { return name; }
-        }
-
-
+        
+        /// <summary>
+        /// Initializes a new instance of the AuctionItem class using the
+        /// provided <code>LuaTable</code> as a data source.
+        /// </summary>
+        /// <param name="data">The scan data to use to build the <code>AuctionItem</code>.</param>
         public AuctionItem(LuaTable data)
         {
-            this.itemData = data;
-
-            name = data[(int)AuctionItemField.Name].ToString();
-            itemWowLink = data[(int)AuctionItemField.WowLink].ToString();
-            itemLevel = Convert.ToInt32(data[(int)AuctionItemField.ItemLevel]);
-            type = data[(int)AuctionItemField.ItemType].ToString();
-            subType = data[(int)AuctionItemField.ItemSubtype].ToString();
-            equipmentSlot = Convert.ToInt32(data[(int)AuctionItemField.EquipmentSlot]);
+            this.name = data[(int)AuctionItemField.Name].ToString();
+            this.itemWowLink = data[(int)AuctionItemField.WowLink].ToString();
+            this.itemLevel = Convert.ToInt32(data[(int)AuctionItemField.ItemLevel]);
+            this.itemType = data[(int)AuctionItemField.ItemType].ToString();
+            this.subType = data[(int)AuctionItemField.ItemSubtype].ToString();
+            this.equipmentSlot = Convert.ToInt32(data[(int)AuctionItemField.EquipmentSlot]);
+        }
+        
+        /// <summary>
+        /// Gets a WoW-formatted string that defines a link to the item in-game.
+        /// </summary>
+        /// <value>
+        /// Gets a WoW-formatted string that defines a link to the item in-game.
+        /// </value>
+        public string ItemWowLink {
+            get {
+                return this.itemWowLink.Replace("|", "\\124");
+            }
+        }      
+        
+        /// <summary>
+        /// Gets the item's level.
+        /// </summary>
+        /// <value>
+        /// Gets the item's level.
+        /// </value>
+        public int ItemLevel {
+            get { return this.itemLevel; }
+        }        
+        
+        /// <summary>
+        /// Gets the main item type.
+        /// </summary>
+        /// <value>
+        /// Gets the main item type.
+        /// </value>
+        public string ItemType {
+            get { return this.itemType; }
+        }
+        
+        /// <summary>
+        /// Gets the item subtype.
+        /// </summary>
+        /// <value>
+        /// Gets the item subtype.
+        /// </value>
+        public string Subtype {
+            get { return this.subType; }
+        }
+        
+        /// <summary>
+        /// Gets the slot the item can occupy.
+        /// </summary>
+        /// <value>
+        /// Gets the slot the item can occupy.
+        /// </value>
+        public int EquipmentSlot {
+            get { return this.equipmentSlot; }
+        }
+        
+        /// <summary>
+        /// Gets the item's name.
+        /// </summary>
+        /// <value>
+        /// Gets the item's name.
+        /// </value>
+        public string Name {
+            get { return this.name; }
         }
     }
 }
